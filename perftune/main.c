6e827e236fce7886deddf669bb6ce0db655cce59
tracer-nju <tracer@nju.edu.cn>
2022-06-28 17:18:26 +0800

    Static hostname: ubuntu        Icon name: computer-vm          Chassis: vm       Machine ID: 8e501a25d3bf4a3eaf132663f1eb7855          Boot ID: b8c35f53d5b74f929c3d217293cf6aab   Virtualization: vmware Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-40-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:18:26 up 12:32,  1 user,  load average: 1.29, 0.51, 0.27

--- perftune/main.c
+++ perftune/main.c
@@ -1,12 +1,22 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <time.h>
 
 int *sieve(int n);
 
 int main() {
+  clock_t start, finish; 
+  double Total_time; 
+
+  start = clock(); 
+
   int *primes = sieve(400);
   for (int *cur = primes, i = 0; *cur; cur++, i++) {
     printf("%8d", *cur);
     if (i % 8 == 7 || !*(cur + 1)) printf("\n");
   }
+
+  finish = clock(); 
+  Total_time = (double)(finish-start) / CLOCKS_PER_SEC; 
+  printf( "%f seconds/n", Total_time); 
 }
