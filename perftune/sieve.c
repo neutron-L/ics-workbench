7e4d4d38027fbad11bed900b5a7ed055ac6ad7f7
tracer-nju <tracer@nju.edu.cn>
2022-06-28 17:25:26 +0800

    Static hostname: ubuntu        Icon name: computer-vm          Chassis: vm       Machine ID: 8e501a25d3bf4a3eaf132663f1eb7855          Boot ID: b8c35f53d5b74f929c3d217293cf6aab   Virtualization: vmware Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-40-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:25:26 up 12:39,  1 user,  load average: 0.64, 0.45, 0.31

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -9,22 +9,38 @@ static bool is_prime[N];
 static int  primes[N];
 
 int *sieve(int n) {
-  assert(n + 1 < N);
-  for (int i = 0; i <= n; i++)
-    is_prime[i] = true;
+  // assert(n + 1 < N);
+  // for (int i = 0; i <= n; i++)
+  //   is_prime[i] = true;
 
 
-  int *p = primes;
-  for (int i = 2; i <= n; i++) {
-    if (is_prime[i])
-    {
-      *p++ = i;
-      for (int j = i * i; j <= n; j += i) {
-        is_prime[j] = false;
-      }
-    }
-  }
-  *p = 0;
+  // int *p = primes;
+  // for (int i = 2; i <= n; i++) {
+  //   if (is_prime[i])
+  //   {
+  //     *p++ = i;
+  //     for (int j = i * i; j <= n; j += i) {
+  //       is_prime[j] = false;
+  //     }
+  //   }
+  // }
+  // *p = 0;
 
+  // return primes;
+  assert(n+1<N);
+  int tot=3;
+  primes[0]=2;
+  primes[1]=3;
+  primes[2]=5;
+	for(int i=6;i<=n;++i){
+    if((i&1)==0) continue;
+    if(i%3==0) continue;
+    if(i%5==0) continue;
+		if(!is_prime[i]) primes[tot++]=i;
+		for(int j=3;j<tot && i*primes[j]<=n;++j){
+			is_prime[i*primes[j]]=true;
+			if(i%primes[j]==0) break;
+		}
+	}
   return primes;
 }
