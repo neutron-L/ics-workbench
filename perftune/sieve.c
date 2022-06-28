a1d4a4b1139d12ad3b392e6b410285dbf23bb25a
tracer-nju <tracer@nju.edu.cn>
2022-06-28 17:13:35 +0800

    Static hostname: ubuntu        Icon name: computer-vm          Chassis: vm       Machine ID: 8e501a25d3bf4a3eaf132663f1eb7855          Boot ID: b8c35f53d5b74f929c3d217293cf6aab   Virtualization: vmware Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-40-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:13:35 up 12:27,  1 user,  load average: 0.27, 0.19, 0.14

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -1,34 +1,3 @@
-// #include <stdbool.h>
-// #include <string.h>
-// #include <assert.h>
-// #include <stdio.h>
-
-// #define N 10000000
-
-// static bool is_prime[N];
-// static int  primes[N];
-
-// int *sieve(int n) {
-//   assert(n + 1 < N);
-//   for (int i = 0; i <= n; i++)
-//     is_prime[i] = true;
-
-//   for (int i = 2; i <= n; i++) {
-//     // if (is_prime[i])
-//       for (int j = i + i; j <= n; j += i) {
-//         is_prime[j] = false;
-//       }
-//   }
-
-//   int *p = primes;
-//   for (int i = 2; i <= n; i++)
-//     if (is_prime[i]) {
-//       *p++ = i;
-//     }
-//   *p = 0;
-//   return primes;
-// }
-
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
@@ -37,37 +6,25 @@
 #define N 10000000
 
 static bool is_prime[N];
-static int primes[N];
+static int  primes[N];
 
-int *sieve(int n)
-{
-  int k = 0;
+int *sieve(int n) {
   assert(n + 1 < N);
-  for (int i = 3; i<= n; i+=2)
+  for (int i = 0; i <= n; i++)
     is_prime[i] = true;
 
-  primes[k++] = 2;    
-
-  for (int i = 3; i<= n; i += 2)
-  {
-    if (is_prime[i])
-    {
-      primes[k++] = i;
-      if (i>3162) continue;
-      for (int j = i * i; j<= n; j +=2*i)
-      {
+  for (int i = 2; i <= n; i++) {
+    // if (is_prime[i])
+      for (int j = i + i; j <= n; j += i) {
         is_prime[j] = false;
       }
-      
-     
-    }
   }
 
-  // int *p = primes; 
-  // for (int i = 2; i <= n; i++)
-  //   if (is_prime[i]) {
-  //     *p++ = i;
-  //   }
-  // *p = 0;
+  int *p = primes;
+  for (int i = 2; i <= n; i++)
+    if (is_prime[i]) {
+      *p++ = i;
+    }
+  *p = 0;
   return primes;
 }
