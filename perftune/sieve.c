383017a4303cb863086fd490f204aed57d9c5796
tracer-nju <tracer@nju.edu.cn>
2022-06-28 17:05:22 +0800

    Static hostname: ubuntu        Icon name: computer-vm          Chassis: vm       Machine ID: 8e501a25d3bf4a3eaf132663f1eb7855          Boot ID: b8c35f53d5b74f929c3d217293cf6aab   Virtualization: vmware Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-40-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:05:22 up 12:19,  1 user,  load average: 0.30, 0.24, 0.14

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -13,12 +13,12 @@ int *sieve(int n) {
   for (int i = 0; i <= n; i++)
     is_prime[i] = true;
 
-  // for (int i = 2; i <= n; i++) {
-  //   if (is_prime[i])
-  //     for (int j = i + i; j <= n; j += i) {
-  //       is_prime[j] = false;
-  //     }
-  // }
+  for (int i = 2; i <= n; i++) {
+    if (is_prime[i])
+      for (int j = i + i; j <= n; j += i) {
+        is_prime[j] = false;
+      }
+  }
 
   int *p = primes;
   for (int i = 2; i <= n; i++)
